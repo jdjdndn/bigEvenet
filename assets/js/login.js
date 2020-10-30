a9b8f8c74826bb077a02ef31d8dbba9852b64870
chengyu <2667389861@qq.com>
2020-10-30 14:43:39 +0800

    表单验证规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,12 @@ $(function () {
     $(".login-box").show()
     $(".reg-box").hide()
   })
+
+
+  let form = layui.form
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+    ]
+  })
 })
