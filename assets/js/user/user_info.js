b6cade06eaa7a1ca4dbcfa1b9273991dfb6909fe
chengyu <2667389861@qq.com>
2020-11-01 15:32:15 +0800

    14

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -6,4 +6,18 @@ $(function () {
       if (value.length <= 6) return '昵称不能少于6位'
     }
   })
+
+  getUserInfo()
+
+  function getUserInfo() {
+    $.ajax({
+      method: 'GET',
+      url: "/my/userinfo",
+      success: function (res) {
+        // console.log(res);
+        //表单对应属性lay-filter='formData'  
+        form.val('formData',res.data)
+      }
+    })
+  }
 })
