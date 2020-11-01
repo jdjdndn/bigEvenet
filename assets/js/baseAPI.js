f67504f2c44919124947981c48adad0683956e76
chengyu <2667389861@qq.com>
2020-11-01 14:18:12 +0800

    11

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,9 @@
 $.ajaxPrefilter(function (options) {
-  console.log(options);
-  options.url='http://ajax.frontend.itheima.net'+options.url
+  // console.log(options);
+  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  if (options.url.indexOf('/my/')) {
+    options.headers= {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+$(function () {
+  //获取用户数据,渲染用户头像,改用户名
+  getUserInfo()
+
+  let layer = layui.layer
+  //退出功能
+  $('#toLoginout').click(function () {
+    console.log(11);
+    layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+      //do something
+      localStorage.removeItem('token')
+      location.href='/login.html'
+      layer.close(index);
+    });
+})
+
+
+  function getUserInfo() {
+    $.ajax({
+      // http://ajax.frontend.itheima.net
+      method: 'GET',
+      url: "/my/userinfo",
+      // headers: {
+      //   Authorization: localStorage.getItem('token') || ''
+      // },
+      success: function (res) {
+        // console.log(res);
+        if(res.status!=0) return layer.msg('获取用户信息失败')
+        renderUser(res.data)
+      }
+    })
+  }
+
+  function renderUser(user) {
+    
+    let name = user.nickname || user.username
+    $('.text-avatar').html('欢迎 &nbsp;&nbsp;'+name)
+    if (user.user_pic) {
+      $('.layui-nav-img').prop('src', user.user_pic).show()
+      $('.text-avatar').hide()
+    } else {
+      let first=name[0].toUpperCase()
+      $('.layui-nav-img').hide()
+      $('.moren').html(first).show()
+    }
+  }
+})
