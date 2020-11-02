948ce8eb18519727efc26379e8c55707753594f6
chengyu <2667389861@qq.com>
2020-11-02 16:13:55 +0800

    21

--- assets/js/artical/art_cate.js
+++ assets/js/artical/art_cate.js
@@ -1,5 +1,8 @@
 $(function () {
+  //获取分类列表并显示
   getArtCatelist()
+  let layer = layui.layer
+  let form = layui.form
 
   function getArtCatelist() {
     $.ajax({
@@ -7,9 +10,85 @@ $(function () {
       url: "/my/article/cates",
       success: function (res) {
         // console.log(res);
-        let html=template('templ',res)
+        let html = template('templ', res)
         $('tbody').html(html)
       }
     })
   }
+  let addindex = ''
+  $('#addCateBtn').on('click', function () {
+    addindex = layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '添加文章分类'
+      , content: $("#addCateTep").html()
+    })
+  })
+
+  //新增分类
+  $(document).on('submit', '#addForm', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: "POST",
+      url: "/my/article/addcates",
+      data: $(this).serialize(),
+      success: function (res) {
+        // console.log(res);
+        if (res.status !== 0) return layer.msg('新增失败')
+        layer.close(addindex)
+        layer.msg('新增成功')
+        getArtCatelist()
+      }
+    })
+  })
+
+  let editindex = ''
+  $(document).on('click', '#editCate', function () {
+    editindex = layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '添加文章分类',
+      content: $("#editCateTep").html()
+    })
+    //获取数据填充表格
+    $.ajax({
+      method: "GET",
+      url: "/my/article/cates/" + $(this).attr('data-id'),
+      success: function (res) {
+        // console.log(res);
+        if (res.status !== 0) return layer.msg('获取文章数据失败')
+        // layer.msg('获取文章数据成功')
+        form.val('editForm', res.data)
+      }
+    })
+  })
+  //修改表单提交
+  $(document).on('submit', '#editForm', function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: "POST",
+      url: "/my/article/updatecate",
+      data: $(this).serialize(),
+      success: function (res) {
+        // console.log(res);
+        if (res.status !== 0) return layer.msg('修改文章数据失败')
+        layer.msg('修改文章数据成功')
+        getArtCatelist()
+        layer.close(editindex)
+      }
+    })
+  })
+
+  //删除当前分类
+  $('tbody').on('click','#removeCate', function () {
+    $.ajax({
+      method: 'GET',
+      url: "/my/article/deletecate/" + $(this).attr('data-id'),
+      success: function (res) {
+        if (res.status !== 0) return layer.msg('删除文章数据失败')
+        layer.msg('删除文章数据成功')
+        getArtCatelist()
+      }
+  })
+})
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -6,43 +6,38 @@ $(function () {
   //退出功能
   $('#toLoginout').click(function () {
     console.log(11);
-    layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
       //do something
       localStorage.removeItem('token')
-      location.href='/login.html'
+      location.href = '/login.html'
       layer.close(index);
     });
+  })
+
+
 })
-
-
-  function getUserInfo() {
-    $.ajax({
-      // http://ajax.frontend.itheima.net
-      method: 'GET',
-      url: "/my/userinfo",
-      // headers: {
-      //   Authorization: localStorage.getItem('token') || ''
-      // },
-      success: function (res) {
-        // console.log(res);
-        if(res.status!=0) return layer.msg('获取用户信息失败')
-        renderUser(res.data)
-      }
-    })
-    return window.getUserInfo=getUserInfo
-  }
-
-  function renderUser(user) {
-    
-    let name = user.nickname || user.username
-    $('.text-avatar').html('欢迎 &nbsp;&nbsp;'+name)
-    if (user.user_pic) {
-      $('.layui-nav-img').prop('src', user.user_pic).show()
-      $('.text-avatar').hide()
-    } else {
-      let first=name[0].toUpperCase()
-      $('.layui-nav-img').hide()
-      $('.moren').html(first).show()
+function getUserInfo() {
+  $.ajax({
+    // http://ajax.frontend.itheima.net
+    method: 'GET',
+    url: "/my/userinfo",
+    success: function (res) {
+      if (res.status != 0) return layer.msg('获取用户信息失败')
+      renderUser(res.data)
     }
+  })
+}
+
+function renderUser(user) {
+
+  let name = user.nickname || user.username
+  $('.text-avatar').html('欢迎 &nbsp;&nbsp;' + name)
+  if (user.user_pic) {
+    $('.layui-nav-img').prop('src', user.user_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    let first = name[0].toUpperCase()
+    $('.layui-nav-img').hide()
+    $('.moren').html(first).show()
   }
-})
+}
